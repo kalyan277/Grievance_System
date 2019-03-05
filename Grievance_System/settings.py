911a34160a6936ea70cbc1fb5b0fc6f8ba405045
kalyan277 <kalyansingh277@gmail.com>
2019-03-05 12:06:17 +0530

    Update settings.py

--- Grievance_System/settings.py
+++ Grievance_System/settings.py
@@ -68,16 +68,7 @@ WSGI_APPLICATION = 'Grievance_System.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ddgphbqn5irif',
-        'USER':'yuvdkyhxoeskcf',
-        'PASSWORD':'be9d9a5a3199c744de91d2ccfd20ab8c8677322d204ed3ca4d970d3409e650c1',
-        'HOST':'ec2-54-235-134-25.compute-1.amazonaws.com',
-        'PORT':'5432',
-    }
-}
+
 
 # DATABASES = {
 #     'default': {
